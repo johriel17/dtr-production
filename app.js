bfbdc412f8cdcba30020d369b66ddeb60d1a63fe
leo johriel rivera <leojohrielrivera17@gmail.com>
2024-03-13 02:12:33 +0800

    asd

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api/employee-attendances', employeeAttendanceRoute)
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'))
+    res.sendFile(path.join(__dirname, "client/dist/index.html"))
 })
 
 mongoose
